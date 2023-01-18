ec7d65d8f456113d620f0f3932232dd7facbfd20
TUNG N KIM <Tungkims@gmail.com>
2023-01-18 00:19:28 -0800

    Add project files.

---
+++ MVCModelBindingRestart/Controllers/StudentController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCModelBindingRestart.Models;
+
+namespace MVCModelBindingRestart.Controllers
+{
+	public class StudentController : Controller
+	{
+		// GET: /Student/Create
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost] // This action is hit after the user submits form data
+		public IActionResult Create(IFormCollection formData)
+		{
+			// Validate all data
+
+
+			Student stu = new()
+			{
+				FirstName = formData["first-name"],
+				LastName = formData["last-name"],
+				DateOfBirth = DateTime.Parse(formData["date-of-birth"]),
+				StudentEmail = formData["school-email"],
+				PhoneNumber = formData["phone-number"]
+			};
+
+
+			// Add to database 
+			// StudentDb.Add(stu);
+
+			// Return a view
+
+			return RedirectToAction("Index", "Home");
+		}
+
+		[HttpGet]
+		public IActionResult CreateWithModelBinding()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult CreateWithModelBinding(Student stu)
+		{
+			if (ModelState.IsValid)
+			{
+				// Add to database 
+				// StudentDB.Add(stu);
+
+				// returns to Index view of HomeController
+				return RedirectToAction("Index", "Home");
+			}
+			// If the ModelState is not valid
+			// show the user the same web page,
+			// with validation errors
+			return View(stu);
+		}
+	}
+}
+
---
+++ MVCModelBindingRestart/Models/Student.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace MVCModelBindingRestart.Models
+{
+	public class Student
+	{
+		[Key] // Make this property primary key
+		public int Id { get; set; }
+
+		/// <summary>
+		/// The legal first name
+		/// </summary>
+		[Display(Name = "First Name")]
+		[Required(ErrorMessage = "{0} is required.")]
+		public string FirstName { get; set; }
+
+		/// <summary>
+		/// The legal last name
+		/// </summary>
+		[Display(Name = "Last Name")]
+		[Required(ErrorMessage = "{0} is required.")]
+		public string LastName { get; set; }
+
+		/// <summary>
+		/// Student's Date of birth
+		/// </summary>
+		[Display(Name = "Date Of Birth")]
+		[Required(ErrorMessage = "{0} is required.")]
+		[DataType(DataType.Date)] // display Date picker
+		public DateTime DateOfBirth { get; set; }
+
+		/// <summary>
+		/// The assigned school email address
+		/// </summary>
+		//[Required()]
+		[Display(Name = "Student Email")]
+		// [DataType(DataType.EmailAddress)]
+		[EmailAddress(ErrorMessage = "Invalid email address.")]
+		public string? StudentEmail { get; set; }
+
+		/// <summary>
+		/// Student's preferred contact number
+		/// </summary>
+		//[Required()]
+		[Display(Name = "Phone Number")]
+		// [DataType(DataType.PhoneNumber)]
+		[Phone]
+		public string? PhoneNumber { get; set; }
+	}
+}
